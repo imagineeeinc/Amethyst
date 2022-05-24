02e6fb5988b8e63e8e4c7cb2844e65b2e6c53a66
Imagineee <imagineeelego@gmail.com>
2022-05-24 18:26:49 +0100

    changed the seek from seconds to minutes

--- src/commands/seek.js
+++ src/commands/seek.js
@@ -7,16 +7,16 @@ module.exports = {
 	.setDescription('seek to a specific time in the current song')
 	.addStringOption(option =>
 		option.setName('time')
-			.setDescription('The time to seek to (in seconds)')
+			.setDescription('The time to seek to (in minutes)')
 			.setRequired(true)),
 	async execute(interaction, player) {
 		const queue = player.getQueue(interaction.guild.id);
 		if (!queue || !queue.playing) return message.channel.send(`No music currently playing... try again ? ❌`);
 		let time = interaction.options.getString('time');
 		if (!time) return interaction.reply(`Please provide a time to seek to... try again ? ❌`);
-		timeTo = ms(time)*1000;
+		timeTo = (ms(time)*60)*1000;
 		const seek = queue.seek(timeTo);
 		if (!seek) return interaction.reply(`Something went wrong... try again ? ❌`);
-		interaction.reply(`Seeking to ${ms(time, { long: true })}s... ✅`);
+		interaction.reply(`Seeking to ${ms(time, { long: true })} minutes... ✅`);
 	},
 };
